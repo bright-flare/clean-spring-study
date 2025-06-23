08cd79fdcf41597972fb33199abe5a264729050c
bright-flare <orolsyeo@gmail.com>
2025-06-23 12:57:34 +0900

    init

---
+++ src/main/java/clean/spring/study/splearn/CleanSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package clean.spring.study.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanSpringStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CleanSpringStudyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/clean/spring/study/splearn/CleanSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package clean.spring.study.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanSpringStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
