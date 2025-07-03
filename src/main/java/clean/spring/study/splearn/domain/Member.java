307d01ff6a2f936f327168f7c4e4c4abfa4858e0
밝은불꽃 (심현섭) <orolsyeo@gmail.com>
2025-07-03 19:33:29 +0900

    Add Member and PasswordEncoder classes with activation/deactivation logic and tests (#2)

--- src/main/java/clean/spring/study/splearn/domain/Member.java
+++ src/main/java/clean/spring/study/splearn/domain/Member.java
@@ -1,10 +1,13 @@
 package clean.spring.study.splearn.domain;
 
 import lombok.Getter;
+import lombok.ToString;
+import org.springframework.util.Assert;
 
 import java.util.Objects;
 
 @Getter
+@ToString
 public class Member {
 
   String email;
@@ -15,11 +18,28 @@ public class Member {
 
   MemberStatus status;
 
-  public Member(String email, String nickname, String passwordHash) {
+  private Member(String email, String nickname, String passwordHash) {
     this.email = Objects.requireNonNull(email);
     this.nickname = Objects.requireNonNull(nickname);
     this.passwordHash = Objects.requireNonNull(passwordHash);
     this.status = MemberStatus.PENDING;
   }
   
+  public static Member create(String email, String nickname, String password, PasswordEncoder passwordEncoder) {
+    return new Member(email, nickname, passwordEncoder.encode(password));
+  }
+  
+  public void activate() {
+    
+    Assert.state(this.status == MemberStatus.PENDING, "PENDING 상태가 아닙니다. 이미 활성화된 회원입니다.");
+    
+    this.status = MemberStatus.ACTIVE;
+  }
+
+  public void deactivate() {
+    
+    Assert.state(this.status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다. 이미 비활성화된 회원입니다.");
+    
+    this.status = MemberStatus.DEACTIVATED;
+  }
 }
